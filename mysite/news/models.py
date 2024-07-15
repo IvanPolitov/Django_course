20582f0fb5309f3bd13e9f76320096353e6cd3e9
IvanPolitov Work <ivan.hanter21@mail.ru>
2024-07-15 11:50:16 +0500

    Study tamplates, models relationship

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -12,6 +12,8 @@ class News(models.Model):
         upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(
         default=False, verbose_name='Опубликовано')
+    category = models.ForeignKey(
+        'Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -20,3 +22,19 @@ class News(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at']
+
+    def hi(self):
+        return "hihihihihihihihihihi"
+
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=150, db_index=True, verbose_name='Категория')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']

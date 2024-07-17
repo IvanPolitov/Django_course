7bea36cfae6c0e2f1aa8ed21f9c6c5514d84271b
IvanPolitov Work <ivan.hanter21@mail.ru>
2024-07-17 12:08:26 +0500

    I studied the Listview, Detailview and Createview

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,44 +1,89 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 
 from .models import News, Category
 from .forms import NewsForm
 
 
-def index(request):
-    news = News.objects.all()
-    categories = Category.objects.all()
-    context = {
-        'news': news,
-        'categories': categories,
-        'title': 'Список новостей',
-    }
-    return render(request, 'news/index.html', context)
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return News.objects.filter(is_published=1)
 
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
-    category = Category.objects.get(pk=category_id)
-    context = {
-        'news': news,
-        'categories': categories,
-        'category': category,
-    }
-    return render(request, template_name='news/category.html', context=context)
+# def index(request):
+#     news = News.objects.all()
+#     categories = Category.objects.all()
+#     context = {
+#         'news': news,
+#         'categories': categories,
+#         'title': 'Список новостей',
+#     }
+#     return render(request, 'news/index.html', context)
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=1)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
 
 
-def view_news(request, news_id):
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {'news_item': news_item})
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     categories = Category.objects.all()
+#     category = Category.objects.get(pk=category_id)
+#     context = {
+#         'news': news,
+#         'categories': categories,
+#         'category': category,
+#     }
+#     return render(request, template_name='news/category.html', context=context)
+
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+
+    # template_name = 'news/news_detail.html'
+    # pk_url_kwarg = 'news_id'
 
 
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+# def view_news(request, news_id):
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {'news_item': news_item})
+
+
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    success_url = reverse_lazy('home')
+
+
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})

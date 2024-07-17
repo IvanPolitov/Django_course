c9501f5ee187bf707c6658c28a7cb19c6b853c41
IvanPolitov Work <ivan.hanter21@mail.ru>
2024-07-17 16:52:04 +0500

    I studied some orm

--- mysite/news/templatetags/news_tags.py
+++ mysite/news/templatetags/news_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from news.models import Category
 
@@ -12,5 +13,6 @@ def get_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_category(a1=0, a2=1):
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {"categories": categories, 'a1': a1, 'a2': a2}
